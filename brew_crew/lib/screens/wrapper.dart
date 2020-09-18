b1e2d7215fd9acc683c108ebc03059e3e71e7b52
matheus-carvalho <matheus.hatzlhoffer@conpec.com.br>
2020-09-18 20:55:47 -0300

    Começando a fazer a autenticação do aplicativo usando signInAnonymously do firebase

---
+++ brew_crew/lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:brew_crew/screens/authentication/authentication.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}

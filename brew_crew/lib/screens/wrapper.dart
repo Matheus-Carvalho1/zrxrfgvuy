34fbbe54d21d1091076271081c6ffc2ab0d5f59a
matheus-carvalho <matheus.hatzlhoffer@conpec.com.br>
2020-09-23 17:51:56 -0300

    Aplicação com log out e começando a fazer o login por e-mail

--- brew_crew/lib/screens/wrapper.dart
+++ brew_crew/lib/screens/wrapper.dart
@@ -1,9 +1,18 @@
 import 'package:brew_crew/screens/authentication/authentication.dart';
+import 'package:brew_crew/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/models/user.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Authenticate();
+    final user = Provider.of<User>(context);
+
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
--- brew_crew/lib/services/auth.dart
+++ brew_crew/lib/services/auth.dart
@@ -1,16 +1,34 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid) : null;
+  }
+
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+//      .map((FirebaseUser user) => _userFromFirebaseUser(user));
+        .map(_userFromFirebaseUser); // essa linha é igual a de cima
+  }
+
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
-      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
       return null;
     }
   }
